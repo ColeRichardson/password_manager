387d913a495ac753ae89bca22e65a4ff125d778c
Alex Lin <linalex7@protonmail.com>
2019-06-13 00:46:31 -0400

    pulling entry data

--- gui.py
+++ gui.py
@@ -1,25 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import tkinter as tk
 from connection import Connection
-#import password_generator
+
+
+# import password_generator
 
 class GUI:
+
     """ a gui for the password manager using tkinter
     """
+
     def __init__(self):
         """initializes the gui, sets up variables.
+        self.status: 'register' or 'logon' or '' """
 
-        self.status: 'register' or 'logon' or ''
-        """
-        self.root = tk.Tk() #initialize tkinter root
+        self.root = tk.Tk()  # initialize tkinter root
 
-        self.account_list = [] # holds account objects in the Gui class
-        self._status = 'register' # holds the status of what window we are in
-        #self.username_entry = ""
-        #self.password_entry = ""
+        self.account_list = []  # holds account objects in the Gui class
+        self._status = 'register'  # holds the status of what window we are in
+
         self.root.title("It's Managed")
-        self.setup_logon() # prompt user with a button to ask to register
+        self.setup_logon()  # prompt user with a button to ask to register
 
-        self.sock = Connection('127.0.0.1', 55555) #comment this line out to run gui without connection
+        self.sock = Connection('127.0.0.1', 55555)
+        # comment this line out to run gui without connection
+
     def setup_logon(self):
         """
         sets up the gui for the logon window where user is prompted for username and password.
@@ -28,7 +35,7 @@ class GUI:
         :return:
         """
 
-        self.root.geometry("400x400")
+        self.root.geometry('400x400')
         username_frame = tk.Frame(self.root)
         username_frame.pack()
         password_frame = tk.Frame(self.root)
@@ -36,35 +43,39 @@ class GUI:
         button_frame = tk.Frame(self.root)
         button_frame.pack()
 
-        username_label = tk.Label(username_frame, text="Username: ")
-        password_label = tk.Label(password_frame, text="Password: ")
-        username_entry = tk.Entry(username_frame)
-        password_entry = tk.Entry(password_frame)
-        login_button = tk.Button(button_frame, text="Login", command=self.login)
-        register_button = tk.Button(button_frame, text="Register", command=self.setup_register)
+        username_label = tk.Label(username_frame, text='Username: ')
+        password_label = tk.Label(password_frame, text='Password: ')
+        self.username_entry = tk.Entry(username_frame)
+        self.password_entry = tk.Entry(password_frame)
+        login_button = tk.Button(button_frame, text='Login',
+                                 command=self.login)
+        register_button = tk.Button(button_frame, text='Register',
+                                    command=self.register)
 
         username_label.pack(side=tk.LEFT)
-        username_entry.pack(side=tk.LEFT)
+        self.username_entry.pack(side=tk.LEFT)
         password_label.pack(side=tk.LEFT)
-        password_entry.pack(side=tk.LEFT)
+        self.password_entry.pack(side=tk.LEFT)
         login_button.pack(side=tk.LEFT)
         register_button.pack(side=tk.LEFT)
 
+    def login(self):
+        user = self.username_entry.get()
+        passwd = self.password_entry.get()
+        print(user)
+        print(passwd)
+        sock_response = self.sock.login(user, passwd)
+        if sock_response == 0:
+            pass
+        else:
+            pass
 
-    def login(self, username: str, password: str):
-		#self.sock.login(username, password)
-        pass
-
-    def setup_register(self):
+    def register(self):
         """sets up the gui to prompt the user to register an account.
-
         :param self:
         :return:
         """
-        self.root.geometry("500x500")
-
-
-
+        pass
 
     def setup_main(self):
         """seets up the gui for the main windows of the application
@@ -73,6 +84,7 @@ class GUI:
         :param self:
         :return:
         """
+
         pass
 
     def setup_account_info(self):
@@ -86,7 +98,8 @@ class GUI:
         :param self:
         :return:
         """
-        Button1 = tk.button() #create a tkinter button
+
+        Button1 = tk.button()  # create a tkinter button
 
     def add_account(self):
         """create an account object for the username and password the user gave.
@@ -103,8 +116,3 @@ class GUI:
         :return:
         """
         pass
-
-
-
-
-
