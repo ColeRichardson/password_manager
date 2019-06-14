0d6ca45f4c57e34058b3a834f44b9e5a933f2a99
Alex Lin <linalex7@protonmail.com>
2019-06-14 00:22:11 -0400

    made gui able to send login info.

--- connection.py
+++ connection.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 
 
@@ -13,5 +14,14 @@ class Connection:
 	def connect(self):
 		self.sock.connect((self.host, self.port))
 
-	def Register(self, data):
+	def register(self, data):
 		self.sock.sendall(bytes(data, encoding='utf-8'))
+
+	def login(self, username: str, passwd: str):
+		cmd = bytes('login', encoding='utf-8')
+		self.sock.send(cmd)
+		time.sleep(0.05)
+		self.sock.send(bytes(username, encoding='utf-8'))
+		time.sleep(0.05)
+		self.sock.send(bytes(passwd, encoding='utf-8'))
+		return 0
